2dffa453c94ad6135cf2519d53267931c2bb26fe
Koner K <konerkalkanel28@gmail.com>
2024-07-09 14:49:18 +0100

    Updating Percolation.java

--- Advanced/AdvancedAlgorithms(Part1)/1-UnionFind/Percolation/src/Percolation.java
+++ Advanced/AdvancedAlgorithms(Part1)/1-UnionFind/Percolation/src/Percolation.java
@@ -24,26 +24,37 @@ public class Percolation {
     static final boolean closed = false;
     static final boolean open = true;
     private int numberOfOpenSites = 0;
-    private final int gridSize;
+    private final int gridLength;
+
 
     // Create an nxn grid, with all sites initially blocked.
     public Percolation(int n){
         if (n<=0){
             throw new IllegalArgumentException("n must not be smaller than one.");
         }else{
-            this.gridSize = n;
-            this.percolationGrid = new boolean[this.gridSize][this.gridSize];
-            for (int i=0; i<this.gridSize; i++){
-                for (int j=0; j<this.gridSize; j++){
+            int virtualGridBlocks = 2;
+            this.weightedQuickUnionUF = new WeightedQuickUnionUF(n+virtualGridBlocks);
+            this.gridLength = n;
+            this.percolationGrid = new boolean[this.gridLength][this.gridLength];
+
+            for (int i = 0; i<this.gridLength; i++){
+                for (int j = 0; j<this.gridLength; j++){
                     this.percolationGrid[i][j] = closed;
                 }
+            }
+            for (int i=0; i<this.gridLength; i++){
+                this.weightedQuickUnionUF.union(0, i+1);
+            }
+
+            for (int i=(gridLength*gridLength); i >= (gridLength*gridLength+1)-4 ; i--){
+                this.weightedQuickUnionUF.union((gridLength*gridLength+1), i+1);
             }
         }
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col){
-        if (row <= 0 || row >= this.gridSize || col<=0 || col >= this.gridSize){
+        if (row <= 0 || row >= this.gridLength || col<=0 || col >= this.gridLength){
             throw new IllegalArgumentException("Index out of range.");
         }else{
             if (this.percolationGrid[row][col] != open){
@@ -55,7 +66,7 @@ public class Percolation {
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col){
-        if (row <= 0 || row >= this.gridSize || col<=0 || col >= this.gridSize){
+        if (row <= 0 || row >= this.gridLength || col<=0 || col >= this.gridLength){
             throw new IllegalArgumentException("Index out of range.");
         }else{
             return this.percolationGrid[row][col];
@@ -64,7 +75,7 @@ public class Percolation {
 
     // is the site (row, col) full (closed)?
     public boolean isFull(int row, int col){
-        if (row <= 0 || row >= this.gridSize || col<=0 || col >= this.gridSize){
+        if (row <= 0 || row >= this.gridLength || col<=0 || col >= this.gridLength){
             throw new IllegalArgumentException("Index out of range.");
         }else{
             return !(this.percolationGrid[row][col]);
@@ -80,7 +91,6 @@ public class Percolation {
     public boolean percolates(){
         return false;
     }
-
 
     // test client (optional)
     public static void main(String[] args){
