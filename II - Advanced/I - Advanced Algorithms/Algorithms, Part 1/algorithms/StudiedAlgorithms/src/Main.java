7405085a5730738f5771fe1cb8f451a743721121
Koner K <konerkalkanel28@gmail.com>
2024-10-24 14:39:29 +0100

    Reorganise Algorithms course code: move percolation code and add ThreeSum code

---
+++ II - Advanced/I - Advanced Algorithms/Algorithms, Part 1/algorithms/StudiedAlgorithms/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ThreeSum threeSum = new ThreeSum();
+            int[] eight_numbers = {30,-40,-20,-10,40,0,10,5};
+            System.out.println(threeSum.brute_force_count(eight_numbers));
+        }
+    }
